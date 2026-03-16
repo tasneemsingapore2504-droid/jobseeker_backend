a5b9ebe94869011ab72ba61480e3f480f1179b44
Hussain <shajapurwalahussain2107@gmail.com>
2026-03-16 12:34:34 +0530

    changes in form for Api connection and admin

--- models/InterviewForm.js
+++ models/InterviewForm.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const intFormSchema = new mongoose.Schema({
-  cname: String,
-  intDate: Date,
-  intTime: String,
-  intPlace: String,
-  candidateName: String,
-  apPos: String,
-  selection: String,
-});
+const intFormSchema = new mongoose.Schema(
+  {
+    cname: String,
+    intDate: String,
+    intTime: String,
+    intPlace: String,
+    candidateName: String,
+    apPos: String,
+    selection: String,
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("InterviewForm", intFormSchema);
--- models/JobPost.js
+++ models/JobPost.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
-const jobPostSchema = new mongoose.Schema({
-  company: String,
-  title: String,
-  description: String,
-  requirement: String,
-  qualification: String,
-  skills: String,
-  salary: Number,
+const jobPostSchema = new mongoose.Schema(
+  {
+    company: String,
+    title: String,
+    description: String,
+    requirement: String,
+    qualification: String,
+    skills: String,
 
-  jobType: String,
-  lastDate: Date,
-  workMode: String,
+    salary: Number,
 
-  country: String,
-  state: String,
-  city: String,
-  link: String,
-});
+    jobType: String,
+    lastDate: String,
+    workMode: String,
+
+    country: String,
+    state: String,
+    city: String,
+    link: String,
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("JobPost", jobPostSchema);
